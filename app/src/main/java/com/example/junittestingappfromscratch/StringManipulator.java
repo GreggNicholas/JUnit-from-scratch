016df3312b63d49858112623b8d70428fba7ef0f
Gregg Nicholas <Greggnicholas@pursuit.org>
2019-02-11 23:13:10 -0500

    commits deconstructedWord method

--- app/src/main/java/com/example/junittestingappfromscratch/StringManipulator.java
+++ app/src/main/java/com/example/junittestingappfromscratch/StringManipulator.java
@@ -46,18 +46,20 @@ public class StringManipulator {
 //    }
 
 
-//    public String deconstructedWord(String word) {
-//        StringBuilder deconstruct = new StringBuilder();
-//
-//        for (int i = 0; i < word.length(); i++) {
-//            if (word.charAt(i) == 'a' || word.charAt(i) == 'e' || word.charAt(i) == 'i' || word.charAt(i) == 'o' || word.charAt(i) == 'u') {
-//                deconstruct.append(word);
-//            } else {
-//
-//            }
-//            return deconstruct.toString();
-//        }
-//    }
+    public String deconstructedWord(String word) {
+        StringBuilder deconstruct = new StringBuilder();
+        StringBuilder consonants = new StringBuilder();
+
+        for (int i = 0; i < word.length(); i++) {
+            if (word.charAt(i) == 'a' || word.charAt(i) == 'e' || word.charAt(i) == 'i' || word.charAt(i) == 'o' || word.charAt(i) == 'u') {
+                deconstruct.append(word.charAt(i));
+            } else {
+                consonants.append(word.charAt(i));
+            }
+        }
+        return deconstruct.toString() + consonants.toString();
+
+    }
 
     public int wordSum(String first, String second) {
 
@@ -65,17 +67,18 @@ public class StringManipulator {
     }
 
 
-    public String longestWord(String[] words){
-        String length ="";
+    public String longestWord(String[] words) {
+        String length = "";
         String longest = "";
 
         for (int i = 0; i < words.length; i++) {
-            if(words[i].length() > longest.length()){
+            if (words[i].length() > longest.length()) {
                 longest = words[i];
 
-            }}
-            return longest;
+            }
         }
+        return longest;
+    }
 
 
 }
